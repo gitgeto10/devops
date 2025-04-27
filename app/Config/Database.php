00e5eea3425c0df379431fed5ccc528c4fb295e2
Aitelbourane_oumaima <oumaimaaitelbourane@gmail.com>
2025-04-27 05:54:41 +0200

    Merge Database.php updated

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -24,32 +24,7 @@ class Database extends Config
      *
      * @var array<string, mixed>
      */
-    public array $default = [
-        'DSN'          => '',
-        'hostname'     => 'localhost',
-        'username'     => 'root',
-        'password'     => '',
-        'database'     => 'devops_db',
-        'DBDriver'     => 'MySQLi',
-        'DBPrefix'     => '',
-        'pConnect'     => false,
-        'DBDebug'      => true,
-        'charset'      => 'utf8mb4',
-        'DBCollat'     => 'utf8mb4_general_ci',
-        'swapPre'      => '',
-        'encrypt'      => false,
-        'compress'     => false,
-        'strictOn'     => false,
-        'failover'     => [],
-        'port'         => 3306,
-        'numberNative' => false,
-        'foundRows'    => false,
-        'dateFormat'   => [
-            'date'     => 'Y-m-d',
-            'datetime' => 'Y-m-d H:i:s',
-            'time'     => 'H:i:s',
-        ],
-    ];
+    public array $default = [];
 
     //    /**
     //     * Sample database connection for SQLite3.
@@ -162,42 +137,70 @@ class Database extends Config
      *
      * @var array<string, mixed>
      */
-    public array $tests = [
-        'DSN'         => '',
-        'hostname'    => '127.0.0.1',
-        'username'    => '',
-        'password'    => '',
-        'database'    => ':memory:',
-        'DBDriver'    => 'SQLite3',
-        'DBPrefix'    => 'db_',  // Needed to ensure we're working correctly with prefixes live. DO NOT REMOVE FOR CI DEVS
-        'pConnect'    => false,
-        'DBDebug'     => true,
-        'charset'     => 'utf8',
-        'DBCollat'    => '',
-        'swapPre'     => '',
-        'encrypt'     => false,
-        'compress'    => false,
-        'strictOn'    => false,
-        'failover'    => [],
-        'port'        => 3306,
-        'foreignKeys' => true,
-        'busyTimeout' => 1000,
-        'dateFormat'  => [
-            'date'     => 'Y-m-d',
-            'datetime' => 'Y-m-d H:i:s',
-            'time'     => 'H:i:s',
-        ],
-    ];
+    public array $tests = [];
+
 
     public function __construct()
     {
         parent::__construct();
 
-        // Ensure that we always set the database group to 'tests' if
-        // we are currently running an automated test suite, so that
-        // we don't overwrite live data on accident.
+        $this->default = [
+            'DSN'          => '',
+            'hostname'     => env('database.default.hostname', 'localhost'),
+            'username'     => env('database.default.username', 'root'),
+            'password'     => env('database.default.password', ''),
+            'database'     => env('database.default.database', 'devops_db'),
+            'DBDriver'     => env('database.default.DBDriver', 'MySQLi'),
+            'DBPrefix'     => env('database.default.DBPrefix', ''),
+            'pConnect'     => false,
+            'DBDebug'      => true,
+            'charset'      => 'utf8mb4',
+            'DBCollat'     => 'utf8mb4_general_ci',
+            'swapPre'      => '',
+            'encrypt'      => false,
+            'compress'     => false,
+            'strictOn'     => false,
+            'failover'     => [],
+            'port'         => 3306,
+            'numberNative' => false,
+            'foundRows'    => false,
+            'dateFormat'   => [
+                'date'     => 'Y-m-d',
+                'datetime' => 'Y-m-d H:i:s',
+                'time'     => 'H:i:s',
+            ],
+        ];
+
         if (ENVIRONMENT === 'testing') {
-            $this->defaultGroup = 'tests';
+            $this->tests = [
+                'DSN'         => '',
+                'hostname'    => env('database.tests.hostname', 'localhost'),
+                'username'    => env('database.tests.username', 'root'),
+                'password'    => env('database.tests.password', ''),
+                'database'    => env('database.tests.database', 'devops_db_test'),
+                'DBDriver'    => env('database.tests.DBDriver', 'MySQLi'),
+                'DBPrefix'    => env('database.tests.DBPrefix', ''),
+                'pConnect'    => false,
+                'DBDebug'     => true,
+                'charset'     => 'utf8',
+                'DBCollat'    => '',
+                'swapPre'     => '',
+                'encrypt'     => false,
+                'compress'    => false,
+                'strictOn'    => false,
+                'failover'    => [],
+                'port'        => 3306,
+                'foreignKeys' => true,
+                'busyTimeout' => 1000,
+                'dateFormat'  => [
+                    'date'     => 'Y-m-d',
+                    'datetime' => 'Y-m-d H:i:s',
+                    'time'     => 'H:i:s',
+                ],
+            ];
+
+            $this->defaultGroup = 'tests'; 
         }
     }
+
 }
